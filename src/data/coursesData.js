ee69fa0847bb0b58accd7bf9526a4dcd5e8b2bdd
hasnain <hasnainzia438@gmail.com>
2025-09-25 14:04:59 +0500

    fixed qaida front img

--- src/data/coursesData.js
+++ src/data/coursesData.js
@@ -295,7 +295,7 @@ export const courses = [
     slug: "qaida-course-beginners",
     name: "Qaida Course Beginners",
     category: "islamic",
-    image: { qaidaFront },
+    image: qaidaFront,
     type: "PAID",
     rating: 4.55,
     learners: "5.8L+",
